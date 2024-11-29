35a899111a0c7b60658be6549147b9d335241a67
Andy Pak <im.andy.pak@gmail.com>
2024-11-28 23:43:35 -0500

    Add displaying board methods

--- Assignment6-Q1/src/main/java/paka4Q1/App.java
+++ Assignment6-Q1/src/main/java/paka4Q1/App.java
@@ -1,10 +1,11 @@
 package paka4Q1;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Board board = new Board();
+        board.setEmptyBoard();
+        board.displayBoard();
     }
 }
--- Assignment6-Q1/src/main/java/paka4Q1/Board.java
+++ Assignment6-Q1/src/main/java/paka4Q1/Board.java
@@ -5,10 +5,36 @@ public class Board {
 
     public Board() {
         this.board = new String[3][3];
-        for (int i = 0; i < this.board.length; i++) {
-            for (int j = 0; j < this.board[i].length; j++) {
+    }
+
+    public int getBoardSize() {
+        return this.board.length;
+    }
+
+    public void setEmptyBoard() {
+        for (int i = 0; i < this.getBoardSize(); i++) {
+            for (int j = 0; j < this.getBoardSize(); j++) {
                 this.board[i][j] = " ";
             }
         }
     }
+
+    public void displayBoard() {
+        for (int i = 0; i < this.getBoardSize(); i++) {
+            for (int j = 0; j < this.getBoardSize(); j++) {
+                System.out.print(" " + this.board[i][j]);
+                if (j < this.getBoardSize() - 1) {
+                    System.out.print(" |");
+                }
+            }
+            System.out.println();
+            if (i < this.getBoardSize() - 1) {
+                System.out.println("---+---+---");
+            }
+        }
+    }
+
+    // TODO loop through to check if winner
+
+    // TODO check if board is full
 }
